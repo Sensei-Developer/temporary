c24d943ca9a8d2c6bac227981597e005c0f2359d
Tejas Singh <tejassingh649@gmail.com>
2023-01-19 12:22:56 +0530

    Trigger A12.1 Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,17 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/XenStuff/manifest --depth 1 -b awaken .repo/local_manifests
+git clone https://github.com/baconpeedit/Local_Manifest.git --depth 1 -b awaken .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch awaken_lavender-userdebug
-export TZ=Asia/Dhaka #put before last build command
-export BUILD_USERNAME=xenxynon
-export BUILD_HOSTNAME=xenxynon
-. rom-build.sh
+lunch awaken_ginkgo-userdebug
+export BUILD_USERNAME=Tejas
+export KBUILD_BUILD_USER=tejas
+export KBUILD_BUILD_HOST=I_Am_Charsi
+export BUILD_HOSTNAME=I_Am_Charsi
+export TZ=Asia/Kolkata
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
