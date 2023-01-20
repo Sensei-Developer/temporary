643d699eea91d61cc63ac7f2911528b7049b3c17
NFS86 <jarbull86@gmail.com>
2023-01-20 10:46:03 +0000

    rosy spark-rom Test

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b spark .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS-Project/local_manifest --depth 1 -b rosy-SparkOS-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch spark_rosy-userdebug
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=rosy
-export BUILD_HOSTNAME=userngebug
-export BUILD_BROKEN_TREBLE_SYSPROP_NEVERALLOW=true
+export BUILD_HOSTNAME=nfsproject
+lunch spark_rosy-userdebug
 mka bacon
 
 # upload rom 
