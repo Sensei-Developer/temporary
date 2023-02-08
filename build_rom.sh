7a9b8ee5fe90da71f328bcbed2a224913e61c2eb
Tejas Singh <tejassingh649@gmail.com>
2023-02-08 14:39:26 +0530

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,12 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch spark_ginkgo-userdebug
-export TZ=Asia/Delhi #put before last build command 
+export BUILD_USERNAME=Tejas
+export KBUILD_BUILD_USER=tejas
+export KBUILD_BUILD_HOST=I_Am_Charsi
+export BUILD_HOSTNAME=I_Am_Charsi
+export TZ=Asia/Kolkata
 mka bacon
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
