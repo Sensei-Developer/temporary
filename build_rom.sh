13bf260b5da77c5216bfd1236fdc75a606db6c34
Tejas Singh <tejassingh649@gmail.com>
2023-02-09 17:18:03 +0530

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -2,10 +2,16 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/baconpeedit/Local_Manifest.git --depth 1 -b evolution .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 # build rom
 source build/envsetup.sh
 lunch evolution_ginkgo-userdebug
-export TZ=Asia/Delhi #put before last build command 
+export BUILD_USERNAME=Tejas
+export KBUILD_BUILD_USER=tejas
+export KBUILD_BUILD_HOST=I_Am_Charsi
+export BUILD_HOSTNAME=I_Am_Charsi
+export TZ=Asia/Kolkata
 mka evolution
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+
+# upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
